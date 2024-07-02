985912accec63f2268578a68b9494e837eaddd23
caroSeminario23 <carolina.seminario@unmsm.edu.pe>
2024-07-01 22:34:11 -0500

    version 1.0.2

--- src/app/components/inicio-sesion/inicio-sesion.component.ts
+++ src/app/components/inicio-sesion/inicio-sesion.component.ts
@@ -1,10 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-inicio-sesion',
   standalone: true,
-  imports: [RouterModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './inicio-sesion.component.html',
   styleUrl: './inicio-sesion.component.css'
 })
